1d3a8fa606cb6685aa9ad4ca5f10ea3cc2f87cac
CoderMahabub <codermahabub@gmail.com>
2021-08-01 23:59:09 +0600

    Updated

--- nested-condition.js
+++ nested-condition.js
@@ -3,18 +3,18 @@ var whiteDanish = true;
 var butterBreadPrice = 25;
 var toastBiscuitPrice = 10;
 var myBudget = 60;
-// if (danishPrice < myBudget) {
-//     console.log('Danish Kheye Danish Jabo');
-// }
-// else if (butterBreadPrice < myBudget) {
-//     console.log('Butter Bon Diye Cha Khamu');
-// }
-// else if (toastBiscuitPrice < myBudget) {
-//     console.log('Toast Biscuit Khabo');
-// }
-// else {
-//     console.log('Budget Eto kom Ja kisui khamu na');
-// }
+if (danishPrice < myBudget) {
+    console.log('Danish Kheye Danish Jabo');
+}
+else if (butterBreadPrice < myBudget) {
+    console.log('Butter Bon Diye Cha Khamu');
+}
+else if (toastBiscuitPrice < myBudget) {
+    console.log('Toast Biscuit Khabo');
+}
+else {
+    console.log('Budget Eto kom Ja kisui khamu na');
+}
 if (danishPrice < myBudget) {
     if (whiteDanish == true) {
         console.log('I will eat');
